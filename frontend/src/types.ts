e9a8adcd2d122792d50cb71cbeca3cf32393fa13
Shreykhanna <khannashrey07@gmail.com>
2025-10-03 09:09:02 +1000

    Add tasks types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,6 +1,19 @@
 export interface Task {
-    id: number;
-    title: string;
-    parentId?: number;
-    subtasks?: Task[];
-  }
+  id: number;
+  title: string;
+  parentId?: number;
+  subtasks?: Task[];
+}
+
+export type ButtonProps = {
+  addSubtasks: (tasksInput: string, parentId: number) => void;
+  task: Task[];
+  tasksInput: string;
+  setSubtasksInput: () => void;
+  label: string;
+};
+
+export type SubTasksInputProps = {
+  subtasksInput: string;
+  setSubtasksInput: () => void;
+};
