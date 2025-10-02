6bd6a2a8226e18b9e086eabebbb43d18b26ec3d7
Shreykhanna <khannashrey07@gmail.com>
2025-10-03 09:08:06 +1000

    Fix backend to support subtasks addition

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,23 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Task } from './entities/tasks.entity';
-import { CreateTaskDto } from './dto/create-task.dto';
+import { Injectable } from "@nestjs/common";
+import { InjectDataSource } from "@nestjs/typeorm";
+import { DataSource, TreeRepository } from "typeorm";
+import { Task } from "./entities/tasks.entity";
+import { CreateTaskDto } from "./dto/create-task.dto";
 
 @Injectable()
 export class TasksService {
-  constructor(@InjectRepository(Task) private tasksRepo: Repository<Task>) {}
+  private readonly tasksRepo: TreeRepository<Task>;
+
+  constructor(@InjectDataSource() private readonly dataSource: DataSource) {
+    this.tasksRepo = this.dataSource.getTreeRepository(Task);
+  }
 
   async create(createTaskDto: CreateTaskDto): Promise<Task> {
     const task = new Task();
     task.title = createTaskDto.title;
+
     if (createTaskDto.parentId) {
-      task.parent = (await this.tasksRepo.findOneBy({ id: createTaskDto.parentId })) ?? undefined;
+      task.parent =
+        (await this.tasksRepo.findOneBy({ id: createTaskDto.parentId })) ??
+        undefined;
     }
+
     return this.tasksRepo.save(task);
   }
 
   async findAll(): Promise<Task[]> {
-    return this.tasksRepo.find({ relations: ['subtasks', 'parent'] });
+    return this.tasksRepo.findTrees();
+  }
+
+  async findOne(id: number): Promise<Task | null> {
+    const task = await this.tasksRepo.findOneBy({ id });
+    return task ? this.tasksRepo.findDescendantsTree(task) : null;
   }
 }
